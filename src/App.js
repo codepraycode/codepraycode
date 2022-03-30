8292dbf7e72395b5f0dfd069fcc2ba2e4720e1a8
Precious Olusola <preciousolusola16@gmail.com>
2022-03-30 13:47:33 +0100

    setup my working envireonment

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Portfolio Site is under development
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={(e)=>e.preventDefault()}
         >
-          Learn React
+          🎉😚🤗
         </a>
       </header>
     </div>
