ecc0ee2163912bf7d4184b37a4a52dc8c96df20b
Precious Olusola <preciousolusola16@gmail.com>
2022-06-12 17:23:38 +0100

    updated the back to top

--- src/widgets/back2top.js
+++ src/widgets/back2top.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const BackToTop = () => {
     return (
-        <button href="#" className="shadow btn-primary rounded-circle back-to-top">
+        <a href="#home" className="shadow btn-primary rounded-circle back-to-top">
             <i className="fas fa-chevron-up"></i>
-        </button>
+        </a>
     );
 };
 
