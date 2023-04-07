f10678afa8b2a1ba860c19e198cbc8208cc7bc90
Precious Olusola <preciousolusola16@gmail.com>
2023-04-07 02:50:30 +0100

    Added particle js to prototype site

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 function App() {
 
   return (
-    <main className="h-full container flex flex--center " id="main">
+    <main className="container flex flex--center " id="main">
 
-      <section className="card flex flex--center flex--column">
+      <section className="card absolute flex flex--center flex--column">
       
         <div className="avatar avatar--md">
           <img  alt="Codepraycode's github avatar" src="https://avatars.githubusercontent.com/u/51521181?s=400&u=4ec6b15bc10506a890bcc35cd6d8bf4e67cb50c7&v=4" />
