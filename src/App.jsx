ec90f2422f88dadc30a3903993ccaea6dd9c9f9d
Precious Olusola <preciousolusola16@gmail.com>
2023-04-07 01:11:21 +0100

    Added social link to prototype upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ function App() {
   return (
     <main className="h-full container flex flex--center flex--column " id="main">
     
-      <h1 className="line-h-1 fs-700 uppercase my-1">Codepraycode!</h1>
+      <h1 className="line-h-1 fs-700 uppercase">Codepraycode!</h1>
+
+      <ul className="socials my-1" role="list">
+        <li>
+          <a href="https://github.com/codepraycode" title="@codepraycode on Github" data-medium="github">
+              <span class="sr-only">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/codepraycode" title="@codepraycode on Linkedin" data-medium="linkedin">
+            <span class="sr-only">Linkedin</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/codepraycode" title="@codepraycode on Twitter" data-medium="twitter">
+              <span class="sr-only">Twitter</span>
+          </a>
+        </li>
+      </ul>
       <a className="btn" href="https://shrtco.de/rE8SdR" role="button">
         View My CV
       </a>
